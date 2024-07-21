129c7c9debc5d2951b79f6011b3c60ea6752c78f
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-21 16:13:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let fName = document.getElementById("fname").value;
+	let lName = document.getElementById("lname").value;
+	alert(`${fName} ${lName}`)
 
 }
